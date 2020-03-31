c824dee30f163f4d731680021c87fe64a137a2e6
asthanaayush25 <asthanaayush25@gmail.com>
2020-03-31 16:40:26 +0530

    commit

---
+++ portfolio/script.js
@@ -0,0 +1,15 @@
+
+   $(document).ready(function(){
+    
+    $(".imgq").delay(1000).slideDown(400);
+    
+    $(".imgx").delay(1000).slideDown(400);
+
+    $(".imgz").delay(1500).slideDown(400);
+    
+
+   
+
+      
+
+   });
